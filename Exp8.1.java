2adf17c26798753fb16cfa7a60a6b8823d7b1333
Jagdish <102206609+7Jags@users.noreply.github.com>
2025-04-17 14:16:25 +0530

    Update Exp8.1.java

--- Exp8.1.java
+++ Exp8.1.java
@@ -19,3 +19,44 @@ It checks the credentials.
 - Compile and deploy the servlet in Tomcat.
 Access the form in your browser:
 http://localhost:8080/login.html
+
+
+
+<!DOCTYPE html>
+<html>
+<head><title>Login</title></head>
+<body>
+<form action="LoginServlet" method="post">
+Username: <input type="text" name="username"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit" value="Login">
+</form>
+</body>
+</html>
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class LoginServlet extends HttpServlet {
+protected void doPost(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+String user = request.getParameter("username");
+String pass = request.getParameter("password");
+response.setContentType("text/html");
+PrintWriter out = response.getWriter();
+if ("admin".equals(user) && "1234".equals(pass)) {
+out.println("<h2>Welcome, " + user + "!</h2>");
+} else {
+out.println("<h2>Login Failed. Invalid credentials.</h2>");
+}
+}
+}
+<web-app>
+<servlet>
+<servlet-name>LoginServlet</servlet-name>
+<servlet-class>LoginServlet</servlet-class>
+</servlet>
+<servlet-mapping>
+<servlet-name>LoginServlet</servlet-name>
+<url-pattern>/LoginServlet</url-pattern>
+</servlet-mapping>
+</web-app>
